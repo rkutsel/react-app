51483a4e122e75c186c0125e32f309909aafc37e
rkutsel <roman.kutsel@docusign.com>
2022-07-15 14:47:11 -0700

    restructured Card && Main components

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,8 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import CardElement from "./components/Card";
 import Footer from "./components/Footer";
 
 const theme = createTheme();
@@ -8,7 +11,10 @@ function App() {
 	return (
 		<>
 			<ThemeProvider theme={theme}>
+				<Navbar />
+				<Hero />
 				<Main />
+				<CardElement />
 				<Footer />
 			</ThemeProvider>
 		</>
---
+++ app/src/components/Card.js
@@ -0,0 +1,51 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export default function CardElement() {
+	return (
+		<>
+			<Container sx={{ py: 8 }} maxWidth="md">
+				<Grid container spacing={4}>
+					{cards.map((card) => (
+						<Grid item key={card} xs={12} sm={6} md={4}>
+							<Card
+								sx={{
+									height: "100%",
+									display: "flex",
+									flexDirection: "column",
+								}}
+							>
+								<CardMedia
+									component="img"
+									sx={{
+										// 16:9
+										pt: "56.25%",
+									}}
+									image="https://source.unsplash.com/random"
+									alt="random"
+								/>
+								<CardContent sx={{ flexGrow: 1 }}>
+									<Typography gutterBottom variant="h5" component="h2">
+										Heading
+									</Typography>
+									<Typography>
+										This is a media card. You can use this section to describe
+										the content.
+									</Typography>
+								</CardContent>
+								<CardActions></CardActions>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+		</>
+	);
+}
