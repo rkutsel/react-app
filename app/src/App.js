ba5f20d11d5e4e6728ea07ea1a129e694d74f78c
rkutsel <roman.kutsel@docusign.com>
2022-07-18 23:12:22 -0700

    Card effects

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,19 @@ import CardElement from "./components/Card";
 import Footer from "./components/Footer";
 
 // const theme = createTheme();
-const cardElements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const cardElements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const urls = {
+	fullStack: "https://github.com/rkutsel/local-exchange-hub",
+	blog: "https://github.com/rkutsel/js-tech-blog",
+	cms: "https://github.com/rkutsel/mysql-cms-builder",
+	noteTaker: "https://github.com/rkutsel/online-note-taker",
+	rev: "https://github.com/rkutsel/rev",
+	dayPlanner: "https://github.com/rkutsel/js-day-scheduler",
+	pwGenerator: "https://github.com/rkutsel/js-password-generator",
+	weatherDash: "https://github.com/rkutsel/js-weather-dashboard",
+};
+
+const imgs = {};
 
 function App() {
 	return (
@@ -15,7 +27,7 @@ function App() {
 			<Navbar />
 			<Hero />
 			<Main />
-			<CardElement cardElements={cardElements} />
+			<CardElement url={urls.fullStack} tag="Fullstack App" />
 			<Footer />
 			{/* </ThemeProvider> */}
 		</>
