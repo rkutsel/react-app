baaf4eff5ee9626791bd04c87da969d04482f6a9
rkutsel <roman.kutsel@docusign.com>
2022-07-19 00:21:19 -0700

    added Link element for Card

--- app/src/components/Card.js
+++ app/src/components/Card.js
@@ -1,35 +1,39 @@
 import Card from "@mui/joy/Card";
-import CardActions from "@mui/material/CardActions";
 import CardCover from "@mui/joy/CardCover";
 import CardContent from "@mui/joy/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/joy/Typography";
-import Container from "@mui/material/Container";
+import imgCover from "../assets/images/code.jpeg";
+import { Link } from "@mui/material";
 
-export default function CardElement({ cardElements }) {
+export default function CardElement({}) {
 	return (
 		<>
 			<Grid item xs={12} sm={6} md={4}>
-				<Card sx={{ minHeight: "270px", minWidth: 270 }}>
-					<CardCover>
-						<img
-							src="https://images.unsplash.com/photo-1542773998-9325f0a098d7?crop=entropy&auto=format&fit=crop&w=3271"
-							alt=""
-						/>
-					</CardCover>
-					<CardCover
+				<Link href="#" color="inherit">
+					<Card
 						sx={{
-							background:
-								"linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)",
+							minHeight: "280px",
+							minWidth: 280,
+							"&:hover": { transform: "scale3d(1.01, 1.01, 1)" },
 						}}
-					/>
-					<CardContent sx={{ justifyContent: "flex-end" }}>
-						<Typography level="h2" fontSize="lg" textColor="#fff" mb={1}>
-							Yosemite National Park
-						</Typography>
-					</CardContent>
-				</Card>
+					>
+						<CardCover>
+							<img src={imgCover} alt="Codebase" />
+						</CardCover>
+						<CardCover
+							sx={{
+								background:
+									"linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)",
+							}}
+						/>
+						<CardContent sx={{ justifyContent: "flex-end" }}>
+							<Typography level="h2" fontSize="lg" textColor="#fff" mb={1}>
+								Yosemite National Park
+							</Typography>
+						</CardContent>
+					</Card>
+				</Link>
 			</Grid>
 		</>
 	);
--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -4,17 +4,14 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import CardElement from "./Card";
 
-const cardElements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 export default function Album() {
 	return (
 		<>
 			<CssBaseline />
 			<Container sx={{ py: 8 }} maxWidth="md">
 				<Grid container spacing={2}>
-					<CardElement cardElements={cardElements} />
-					<CardElement cardElements={cardElements} />
-					{/* <Grid item xs={12} sm={6} md={4}></Grid> */}
+					<CardElement />
+					<CardElement />
 				</Grid>
 			</Container>
 		</>
